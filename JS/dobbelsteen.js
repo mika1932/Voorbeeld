6e014245cecb6159b3b7a41ca1a1b19db72146f8
mika1932 <mika1932@users.noreply.github.com>
2019-09-30 15:35:33 +0200

    bijna gevonden

---
+++ JS/dobbelsteen.js
@@ -0,0 +1,6 @@
+var btnDobbelsteen = document.getElementById("btnDobbelsteen");
+var uitvoerDobbelsteen = document.getElementById("uitvoerDobbelsteen");
+
+btnDobbelsteen.addEventListener("click", function(){
+   console.log(math.random()); 
+});
