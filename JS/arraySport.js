e8bcaa455db97d7133e13e1e35e8d1d1399dbca7
mika1932 <mika1932@users.noreply.github.com>
2019-10-14 14:29:55 +0200

    array afgewerkt

--- JS/arraySport.js
+++ JS/arraySport.js
@@ -1,33 +1,48 @@
-var sportsArray = ["volleybal", "basketbal", "baseball", "tennis", "paddle", "voetbal", "atletiek", "ballet", "dansen", "rugby", "e-sport", "schaken", "paardrijden", "skaten", "mountainbike", "boksen", "zwemmen", "badminton"];
-var balsportArray = ["volleybal", "basketbal", "baseball", "tennis", "paddle", "voetbal", "rugby"];
-var geenBalsportArray = ["atletiek", "ballet", "dansen", "e-sport", "schaken", "paardrijden", "skaten", "mountainbike", "boksen", "zwemmen", "badminton"];
-
-var sportKeuze = document.getElementById("sportKeuze");
-var btnBalsport = document.getElementById("btnBalsport");
-var btnGeenBalsport = document.getElementById("btnGeenBalsport");
-
-//functie uitvoeren wanneer de pagina volledig is geladen
-window.onload = function() {
-    //een willekeurig getal kiezen tussen 0 en de lengte van de sportsArray(0-17)
-    let random = Math.floor(Math.random() * sportsArray.length);
-    //willekeurig element uit sportsArray in sportKeuze plaatsen
-    sportKeuze.innerHTML = sportsArray[random];
-}
-
-btnBalsport.addEventListener("click", function() {
+btnBallsport.addEventListener("click", function(){
+    //de sport uit het element sportKeuze eruithalen
     let sport = sportKeuze.innerHTML;
-    for(var i = 0; i < balsportArray.length; i++){
-        if(sport === balsportArray[i]){
-            alert("goed zo!")
+    //variabele om bij te houden of we juist hebben gekozen 
+    let juistGekozen = false;
+    //doorlopen van heel de balsportArray 
+    for (var i = 0; i < balsportArray.length; i++) {
+        //elk element uit balsportArray controleren tov de sport die in sportKeuze staat 
+        if(sport === balsportArray[i]) {
+            //we hebben juist gekozen 
+            juistGekozen = true;
+
         }
     }
+    if(juistGekozen) {
+         //pop-up om te laten weten dat we goed hebben gekozen
+            alert("Goed zo")
+    }
+    else {
+        alert("Probeer opnieuw!")
+    }
+    willekeurigeSportKiezen();
+
 });
 
-btnGeenBalsport.addEventListener("click", function() {
+btnGeenBalsport.addEventListener("click", function(){
+ //de sport uit het element sportKeuze eruithalen
     let sport = sportKeuze.innerHTML;
-     for(var i = 0; i < geenBalsportArray.length; i++){
-        if(sport === geenBalsportArray[i]){
-            alert("YOU TRASH!")
+    //variabele om bij te houden of we juist hebben gekozen 
+    let juistGekozen = false;
+    //doorlopen van heel de balsportArray 
+    for (var i = 0; i < geenBalsportArray.length; i++) {
+        //elk element uit balsportArray controleren tov de sport die in sportKeuze staat 
+        if(sport === geenBalsportArray[i]) {
+            //we hebben juist gekozen 
+            juistGekozen = true;
+
         }
-     }
+    }
+    if(juistGekozen) {
+         //pop-up om te laten weten dat we goed hebben gekozen
+            alert("Goed zo")
+    }
+    else {
+        alert("Probeer opnieuw!")
+    }
+    willekeurigeSportKiezen();
 });
